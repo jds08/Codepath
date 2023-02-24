2bb3d5af87e8a17caa9e106efbfbf5a9363a3e62
Jonathan S <jsuconota8@gmail.com>
2023-02-24 18:58:45 -0500

    first commit

---
+++ lab1/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+const App = () => {
+  return (
+    <div className="App">
+    <h1>California Trip</h1>
+    <h2>Hey hope you enjoy your weekend in LA </h2>
+    <Calendar/>
+    </div>
+  )
+
+
+
+}
+export default App;
